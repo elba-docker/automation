c3778b36994d652296a4a676943849cbc51dad75
Joseph Azevedo <joseph.az@gatech.edu>
2020-04-24 13:44:40 -0400

    chore: add editorconfig; bump test replicas

--- scripts/build_all_archives.sh
+++ scripts/build_all_archives.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Script to build aggregate archives from multiple tests (for all types)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-bash ./build_archive.sh ".." "d-c-50 d-c-100 d-m-50 d-m-100 d-mc-50 d-mc-100 d-r-50 d-r-100 d-rc-50 d-rc-100"
-bash ./build_archive.sh ".." "i-c-50 i-c-100 i-m-50 i-m-100 i-mc-50 i-mc-100 i-r-50 i-r-100 i-rc-50 i-rc-100"
-bash ./build_archive.sh ".." "ii-c-b ii-c-b ii-m-b ii-m-b ii-mc-b ii-mc-b ii-r-b ii-r-b ii-rc-b ii-rc-b"
-bash ./build_archive.sh ".." "ii-c-s ii-c-s ii-m-s ii-m-s ii-mc-s ii-mc-s ii-r-s ii-r-s ii-rc-s ii-rc-s"
+bash $DIR/build_archive.sh "." "d-c-50 d-c-100 d-m-50 d-m-100 d-mc-50 d-mc-100 d-r-50 d-r-100 d-rc-50 d-rc-100"
+bash $DIR/build_archive.sh "." "i-c-50 i-c-100 i-m-50 i-m-100 i-mc-50 i-mc-100 i-r-50 i-r-100 i-rc-50 i-rc-100"
+bash $DIR/build_archive.sh "." "ii-c-b ii-c-b ii-m-b ii-m-b ii-mc-b ii-mc-b ii-r-b ii-r-b ii-rc-b ii-rc-b"
+bash $DIR/build_archive.sh "." "ii-c-s ii-c-s ii-m-s ii-m-s ii-mc-s ii-mc-s ii-r-s ii-r-s ii-rc-s ii-rc-s"
--- scripts/build_archive.sh
+++ scripts/build_archive.sh
@@ -5,7 +5,8 @@
 TEST_PATH="$1"
 TEST_NAMES="$2"
 for TEST_NAME in $TEST_NAMES; do
-    TAR_PATH="$TEST_PATH/archive/$TEST_NAME.tar.gz"
-    echo "Building archive for $TEST_NAME at $TAR_PATH"
-    tar -czvf $TAR_PATH $TEST_PATH/config.yml $TEST_PATH/logs/$TEST_NAME-*.log $TEST_PATH/results/$TEST_NAME-*.tar.gz $TEST_PATH/working/$TEST_NAME-*
+  mkdir -p $TEST_PATH/archive
+  TAR_PATH="$TEST_PATH/archive/$TEST_NAME.tar.gz"
+  echo "Building archive for $TEST_NAME at $TAR_PATH"
+  tar -czvf $TAR_PATH $TEST_PATH/config.yml $TEST_PATH/logs/$TEST_NAME-*.log $TEST_PATH/results/$TEST_NAME-*.tar.gz $TEST_PATH/working/$TEST_NAME-*
 done
